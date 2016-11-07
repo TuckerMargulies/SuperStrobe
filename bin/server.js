e9e4ef3d1f5813ff64b34454e749b865378bc5eb
James Turle <james@foyerlive.com>
2016-11-07 12:52:46 -0500

    done

---
+++ bin/server.js
@@ -0,0 +1,19 @@
+import Primus from 'primus';
+import http from 'http';
+
+let server = http.createServer((req,res) => {
+  res.setHeader('Content-Type', 'text/html');
+  require('fs').createReadStream(__dirname + '/../pub/index.html').pipe(res);
+});
+
+let primus = new Primus(server);
+
+primus.on('connection', function connection(spark) {
+  spark.on('data', function received(data) {
+    primus.write(data);
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Open http://localhost:8080 in your browser');
+});
