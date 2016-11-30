a07a248d22ecf84c091fc6e868609ed2dee85d77
Tucker Margulies <marguliw@gmail.com>
2016-11-30 14:13:08 -0500

    rolling back changes

--- bin/router.js
+++
@@ -1,6 +0,0 @@
-var express           = require ('express'),
-    server            = express (),
-
-
-
-server.use(express.static('./pub'))
--- bin/server.js
+++ bin/server.js
@@ -1,18 +1,10 @@
 import Primus from 'primus';
 import http from 'http';
-<<<<<<< HEAD
-// import router from ('../bin/router.js'),
-=======
->>>>>>> 6ef2cc705e12499ba93d00f41597a87e17e94710
 
 let server = http.createServer((req,res) => {
   res.setHeader('Content-Type', 'text/html');
   require('fs').createReadStream(__dirname + '/../pub/index.html').pipe(res);
-<<<<<<< HEAD
-  require('fs').createReadStream(__dirname + '/../pub/show.html').pipe(res);
-  res.render('/show')
-=======
->>>>>>> 6ef2cc705e12499ba93d00f41597a87e17e94710
+
 });
 
 let primus = new Primus(server);
@@ -23,16 +15,7 @@ primus.on('connection', function connection(spark) {
   });
 });
 
-<<<<<<< HEAD
-
 
 server.listen(8080, function () {
   console.log('Open http://localhost:8080 in your browser');
 });
-
-// server.use(router);
-=======
-server.listen(8080, function () {
-  console.log('Open http://localhost:8080 in your browser');
-});
->>>>>>> 6ef2cc705e12499ba93d00f41597a87e17e94710
