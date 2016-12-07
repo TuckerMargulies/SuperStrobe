234f62d841f0ccd5a12bf9106f4beb448f9e9579
Tucker Margulies <marguliw@gmail.com>
2016-12-06 19:37:36 -0500

    add routing

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,25 @@
 import Primus from 'primus';
 import http from 'http';
+
 var PORT = process.env.PORT || 8080
 
 let server = http.createServer((req,res) => {
   res.setHeader('Content-Type', 'text/html');
-  require('fs').createReadStream(__dirname + '/../pub/index.html').pipe(res);
+console.log(res.URL)
+switch( req.uri.pathname )
+{
+  case '/touch':
+    require('fs').createReadStream(__dirname + '/../pub/touch.html').pipe(res);
+    break;
+  case '/control':
+    require('fs').createReadStream(__dirname + '/../pub/control.html').pipe(res);
+    break;
+  default:
+    require('fs').createReadStream(__dirname + '/../pub/index.html').pipe(res);
+    break;
+}
 });
+
 
 let primus = new Primus(server);
 
